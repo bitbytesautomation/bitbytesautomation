d287dfc2c573fd2758f0c86ede91b2e75007baa0
bitbytesautomation <bitbytesautomation@proton.me>
2026-04-15 11:40:46 -0700

    Add files via upload

---
+++ assets/js/site-config.js
@@ -0,0 +1,12 @@
+window.BITBYTES_CONFIG = {
+  webhooks: {
+    contact: "https://hook.us2.make.com/t4xvitutoqatbmic4ubmklgykotide6e",
+    onboarding: "https://hook.us2.make.com/jht32w8hdv5sm11or9s94k21t6pcnu4g"
+  },
+  stripe: {
+    setupOneTime: "https://buy.stripe.com/cNi6oA8FddAD6LQgJg83C01",
+    monthly: "#ADD_MONTHLY_600_LINK",
+    sixMonth: "#ADD_6_MONTH_DISCOUNT_LINK",
+    twelveMonth: "#ADD_12_MONTH_DISCOUNT_LINK"
+  }
+};
